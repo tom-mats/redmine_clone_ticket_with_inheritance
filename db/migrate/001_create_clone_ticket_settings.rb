654aabc41a6f3bda18c51c9af83cbc42fcd932d6
tom-mats <dawn.gray.sky@gmail.com>
2016-12-12 23:37:02 +0900

    Add settings hooks

--- db/migrate/001_create_clone_ticket_settings.rb
+++ db/migrate/001_create_clone_ticket_settings.rb
@@ -2,6 +2,7 @@ class CreateCloneTicketSettings < ActiveRecord::Migration
   def change
     create_table :clone_ticket_settings do |t|
       t.integer :project_id
+      t.integer :src_tracker_id
       t.integer :dst_project_id
       t.integer :dst_tracker_id
       t.boolean :copy_attachment
