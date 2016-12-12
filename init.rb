59e5a405d39b4af94027eeacf19db5d233911f39
T Matsushima <dawn.gray.sky@gmail.com>
2016-12-12 13:34:47 +0000

    Add keep file

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_clone_ticket_with_inheritance do
+  name 'Redmine Clone Ticket With Inheritance plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
