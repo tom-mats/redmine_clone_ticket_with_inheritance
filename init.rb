60d8a504844afeb49bcd22731e820d9753e5f3c9
tom-mats <dawn.gray.sky@gmail.com>
2016-12-15 00:35:06 +0900

    Add require

--- init.rb
+++ init.rb
@@ -7,6 +7,11 @@ Redmine::Plugin.register :redmine_clone_ticket_with_inheritance do
   author_url 'http://example.com/about'
 end
 
+Dir[File.expand_path('lib/redmine_clone_ticket_with_inheritance', __FILE__) << '/*,rb'].each do |file|
+  puts file
+  require_dependency file
+end
+
 Rails.configuration.to_prepare do
   require_dependency 'projects_helper'
   unless ProjectsHelper.included_modules.include? CloneTicketProjectsHelperPatch
