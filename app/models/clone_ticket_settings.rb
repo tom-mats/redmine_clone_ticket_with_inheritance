a5a1e1482d4d5e8d2fac860ac3e9ed56897a36e4
tom-mats <dawn.gray.sky@gmail.com>
2016-12-15 00:59:11 +0900

    Temp comment out

--- app/models/clone_ticket_settings.rb
+++ app/models/clone_ticket_settings.rb
@@ -5,8 +5,8 @@ class CloneTicketSettings < ActiveRecord::Base
   validates_uniqueness_of :project_id
   validates_presence_of :project_id
 
-  safe_attributes 'dst_project_id', 'dst_tracker_id', 'copy_attachment', 'copy_children', 'copy_related', 'back_to_status'
-  attr_accessible :dst_project_id, :dst_tracker_id, :copy_attachment, :copy_children, :copy_related, :back_to_status
+  #safe_attributes 'dst_project_id', 'dst_tracker_id', 'copy_attachment', 'copy_children', 'copy_related', 'back_to_status'
+  #attr_accessible :dst_project_id, :dst_tracker_id, :copy_attachment, :copy_children, :copy_related, :back_to_status
 
   def find_or_create(project_id)
     own_setting = CloneTicketSettings.where(project_id: project_id).first
