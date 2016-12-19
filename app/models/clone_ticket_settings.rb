2bf14da1b95889aaafc890d7009fa851f8a70fcd
tom-mats <dawn.gray.sky@gmail.com>
2016-12-20 00:58:55 +0900

    delete test code

--- app/models/clone_ticket_settings.rb
+++ app/models/clone_ticket_settings.rb
@@ -7,7 +7,6 @@ class CloneTicketSettings < ActiveRecord::Base
   attr_accessible :project_id, :dst_project_id, :dst_tracker_id, :copy_attachment, :copy_children, :copy_related, :back_to_status
 
   def self.find_or_create(project_id)
-    p Tracker.all.sort
     own_setting = CloneTicketSettings.find_by(project_id: project_id)
     unless own_setting
       own_setting = CloneTicketSettings.new
