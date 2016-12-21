0c0d81d11cd6ed808e33f21d5aaba8f1a2a40fc4
tom-mats <dawn.gray.sky@gmail.com>
2016-12-22 05:59:18 +0900

    add copy process

--- lib/redmine_clone_ticket_with_inheritance/hooks.rb
+++ lib/redmine_clone_ticket_with_inheritance/hooks.rb
@@ -1,10 +1,16 @@
 module CloneTicketIssuesHooks
   class Hooks < Redmine::Hook::Listener
     def controller_issues_edit_before_save(context={})
-
+      new_issue = copy_from(context[:issue])
     end
     def controller_issues_bulk_edit_before_save(context={})
-      
+      new_issue = copy_from(context[:issue])
+    end
+
+    private
+    def copy_ticket(org_issue)
+      new_issue = Issue.copy_from(org_issue)
+      new_issue
     end
   end
 end
