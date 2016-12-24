75ebbb9e0b1e7e70afd958361eee1d1f9a7f315f
tom-mats <dawn.gray.sky@gmail.com>
2016-12-24 16:21:52 +0900

    copy ticket supported

--- db/migrate/001_create_clone_ticket_settings.rb
+++ db/migrate/001_create_clone_ticket_settings.rb
@@ -8,7 +8,7 @@ class CreateCloneTicketSettings < ActiveRecord::Migration
       t.integer :dst_tracker_id
       t.boolean :copy_attachment
       t.integer :copy_children
-      t.integer :copy_related
+      t.integer :clear_related
       t.boolean :back_to_status
     end
   end
--- lib/redmine_clone_ticket_with_inheritance/hooks.rb
+++ lib/redmine_clone_ticket_with_inheritance/hooks.rb
@@ -1,16 +1,29 @@
 module CloneTicketIssuesHooks
   class Hooks < Redmine::Hook::Listener
     def controller_issues_edit_before_save(context={})
-      new_issue = copy_from(context[:issue])
+      copy_issue(context[:issue])
     end
     def controller_issues_bulk_edit_before_save(context={})
-      new_issue = copy_from(context[:issue])
+      copy_issue(context[:issue])
     end
 
     private
-    def copy_ticket(org_issue)
-      new_issue = Issue.copy_from(org_issue)
-      new_issue
+    def copy_issue(org)
+      @settings = CloneTicketSettings.find_or_create(org.project_id)
+      return unless @settings
+      copied = Issue.new.copy_from(org, {:attachment => @settings[:copy_attachment]})
+      copied.project_id = @settings.dst_project_id if @settings.dst_project_id
+      copied.tracker_id = @settings.dst_tracker_id if @settings.dst_tracker_id
+      copied.custom_field_values = org.custom_field_values.inject({}){ |h,v| h[v.custom_field_id] = v.value; h}
+      if @settings.back_to_status
+        org_before = Issue.find(org_issue.id)
+        org.status_id = org_before.status_id if org_old_issue
+      end
+      if @settings.clear_related
+        org_issue.relations_from.clear
+        org_issue.relations_to.clear
+      end
+      copied.save!
     end
   end
 end
